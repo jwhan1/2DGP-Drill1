68a8cbc91fb10125e9a214e608879e37798ce024
jwhan1 <96102340+jwhan1@users.noreply.github.com>
2024-09-15 23:07:58 +0900

    fix: ControlTurtle에서 실행시 응답하지 않는 이슈 수정
    input 명령어를 제거 및 필요 없는 반복문 제거
    
    #2

--- ControlTurtle.py
+++ ControlTurtle.py
@@ -1,29 +1,33 @@
 import turtle
 turtle.shape("turtle")
 
-arrow = input()
-turtle.setheading(90)
 #루프
 def forwardmove():
     turtle.setheading(90)
+    turtle.forward(50)
+    turtle.stamp()
 def leftmove():
-   turtle.setheading(180)
+    turtle.setheading(180)
+    turtle.forward(50)
+    turtle.stamp()
 def backmove():
     turtle.setheading(270)
+    turtle.forward(50)
+    turtle.stamp()
 def rightmove():
     turtle.setheading(0)
+    turtle.forward(50)
+    turtle.stamp()
 def drawclear():
     turtle.home()
     turtle.clear()
-    turtle.setheading(90)
-while 1:
+
 #방향전환 
-    turtle.onkey(forwardmove,'w')
-    turtle.onkey(leftmove,'a')
-    turtle.onkey(backmove,'s')
-    turtle.onkey(rightmove,'d')
-    turtle.onkey(drawclear, 'Escape')
+turtle.onkey(forwardmove,"w")
+turtle.onkey(leftmove,"a")
+turtle.onkey(backmove,"s")
+turtle.onkey(rightmove,"d")
+turtle.onkey(drawclear, "Escape")
 #전진하고 입력
-    turtle.forward(50)
-    turtle.stamp()
-    turtle.listen()
+
+turtle.listen()
