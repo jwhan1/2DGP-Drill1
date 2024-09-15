2b6f318922f8a778a8ab4a0848f3da538aa46c33
jwhan1 <96102340+jwhan1@users.noreply.github.com>
2024-09-15 22:57:21 +0900

    feat: 거북이 이동 기능 구현
    실행하면 응답 없음이 뜸 수정 필요

--- ControlTurtle.py
+++ ControlTurtle.py
@@ -1,2 +1,29 @@
 import turtle
 turtle.shape("turtle")
+
+arrow = input()
+turtle.setheading(90)
+#루프
+def forwardmove():
+    turtle.setheading(90)
+def leftmove():
+   turtle.setheading(180)
+def backmove():
+    turtle.setheading(270)
+def rightmove():
+    turtle.setheading(0)
+def drawclear():
+    turtle.home()
+    turtle.clear()
+    turtle.setheading(90)
+while 1:
+#방향전환 
+    turtle.onkey(forwardmove,'w')
+    turtle.onkey(leftmove,'a')
+    turtle.onkey(backmove,'s')
+    turtle.onkey(rightmove,'d')
+    turtle.onkey(drawclear, 'Escape')
+#전진하고 입력
+    turtle.forward(50)
+    turtle.stamp()
+    turtle.listen()
